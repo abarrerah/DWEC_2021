270da7a98b853f7d7f303f384391fcfbcc517a6e
abarrerah <alum.abarrerah@iesalixar.org>
2020-10-09 17:48:36 +0200

    hola

--- tema2/U2T6/ejercicio.js
+++ tema2/U2T6/ejercicio.js
@@ -1,24 +1,34 @@
-function getRandomNumber() {
-  return Math.floor(Math.random() * (100 - 1) + 1);
-}
-var result = prompt("Please, enter a number", "1");
-var count = 1;
-var boolean = false;
-number = getRandomNumber();
-while (Boolean != true) {
-  if (result != number) {
+
+var result=prompt("Please, enter a number","1");
+
+
+var random=Math.floor(Math.random()*(100-1)+1);
+var count;
+var bol=false;
+while(bol !=true){
+  if(isNaN(result)){
+    console.log("No es un número ");
+    bol==false;
+    break;
+}else{
+  console.log("Es número");
+  if(result !=random && result<random ){
+    console.log("No es correcto. Es menor");
+    var result=prompt("Please, enter a number","1");
     count++;
-    if (result < number) {
-      console.log("the number is higher");
-    } else {
-      console.log("the number is minor");
-    }
-    var result = prompt("Please, enter a number", "1");
+    bol==false;
     break;
-  } else if (result == number) {
-    console.log("Take this: :D  ");
-    boolean == true;
-    console.log(count);
+  }else if(result !=random && result>random){
+    console.log("No es correcto. Es mayor");
+    var result=prompt("Please, enter a number","1");
+    count++;
+    bol==false;
     break;
+  }else{
+    console.log("Es correcto. Turnos: "+count);
+    bol==true;
   }
 }
+}
+
+
