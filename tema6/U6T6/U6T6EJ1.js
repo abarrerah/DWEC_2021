a6308be5b69474b08e60779f695d7ae1d9f805c2
abarrerah <alum.abarrerah@iesalixar.org>
2021-01-28 21:08:36 +0100

    adding table

--- tema6/U6T6/U6T6EJ1.js
+++ tema6/U6T6/U6T6EJ1.js
@@ -9,10 +9,24 @@ function mostrar() {
   fetch("http://localhost:8090/tema6/U6T6/listar_series.php")
     .then((response) => response.json())
     .then((data) => {
-        
         let alumnos = JSON.parse(JSON.stringify(data));
-    console.log(alumnos);
-    document.getElementById("mostrar").innerHTML=alumnos;
+        let series="<table><tr><th>Title</th><th>Channel</th><th>Director</th><th>Year</th><th>Ended</th></tr></table>"
+
+        for(let i=0; i<alumnos.length; i++){
+            series+="<tr><td>";
+            series+=alumnos[i].titulo;
+            series+="</td><td>";
+            series+=alumnos[i].director;
+            series+="</td><td>";
+            series+=alumnos[i].cadena;
+            series+="</td><td>";
+            series+=alumnos[i].anyo;
+            series+="</td><td>";
+            series+=alumnos[i].terminada;
+            series+="</td></tr>";
+        }
+        series+="</table>";
+        document.getElementById("mostrar").innerHTML=series;
     })
     .catch((err) => console.log(err));
     
